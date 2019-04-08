bec09f102133ee6d91e3d68674fee3c415c4592d
Литвинов <litvinovMS@studio-tg.ru>
2019-04-08 15:01:55 +0300

    Add API response logger.

--- RiotAPI/v4/summoner/byName.js
+++ RiotAPI/v4/summoner/byName.js
@@ -5,6 +5,8 @@ const getSummonerByName = async name => {
   logger.log('API', 'Calling summoner by name.');
   const APIResponse = await requestAPI(`summoner/v4/summoners/by-name/${name}`, 'get');
   const { data, status, errMessage } = APIResponse;
+  logger.log('API', `Got API response with status ${status}${status === 1 ? ` and error message ${errMessage}` : ''}:`);
+  logger.log('API', JSON.stringify(data));
   if (status === 1) {
     logger.log('error', `Error in API handler: ${errMessage}.`);
     throw new Error(`Error accessing RIOT API: ${errMessage}`);
